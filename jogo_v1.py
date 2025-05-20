f20162f7811b0be70631551f0070af08ebb7241f
Estela <estelabs@edu.insper.edu.br>
2025-05-20 17:20:44 -0300

    Ajuste dos preçõs do mundo, e inseri as suas posições. Comentarios e mais alguns detalhes

--- jogo_v1.py
+++ jogo_v1.py
@@ -36,8 +36,8 @@ background_inicio = pygame.image.load(os.path.join(caminho_img, 'background.png'
 background_inicio = pygame.transform.scale(background_inicio, (WIDTH, HEIGHT))
 
 # Cronometro
-fonte = pygame.font.SysFont("arial", 36)
-tempo_inicial = pygame.time.get_ticks()  # em milissegundos
+fonte = pygame.font.SysFont("arial", 36)  # inicia a fonte que cronometro ira usar 
+tempo_inicial = pygame.time.get_ticks()  # Faz o tempo rodar como um relogio
 tempo_total = 60 * 1000  # 60 segundos em milissegundos
 
 #Botão menu 
@@ -124,6 +124,7 @@ diamond_verde_broken_img = pygame.transform.scale(diamond_verde_broken_img, (80,
 # Moeda
 coin_img = pygame.image.load(os.path.join(caminho_img, 'coin.png')).convert_alpha()
 coin_img = pygame.transform.scale(coin_img, (40, 40))
+fonte_moeda = pygame.font.SysFont(None, 28)  #configura valor da moeda
 
 # Pedra
 pedra_img = pygame.image.load(os.path.join(caminho_img, 'pedra.png')).convert_alpha()
@@ -195,11 +196,11 @@ botao2_rect = botao2_img.get_rect(center=(WIDTH // 2, HEIGHT // 2))
 botao3_rect = botao3_img.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 140))
 
 
-#dicionario com os custos dos mundos
+#dicionario com os custos dos mundos, e define eles como bloqueados, sendo apenas desbloqueados quando o jogador compra
 mundos_desbloqueio = {
     "mundo_planeta": {"custo": 250, "desbloqueado": False},
     "mundo_mistico": {"custo": 750, "desbloqueado": False},
-    "mundo_verde": {"custo": 1200, "desbloqueado": False}
+    "mundo_verde": {"custo": 1000, "desbloqueado": False}
 }
 
 #classe de diamante 
@@ -213,7 +214,7 @@ class Diamante(pygame.sprite.Sprite):
             self.image_normal = orbe_img 
             self.image_broken = orbe_img  
             velocidade_extra = 5 #aumenta a velocidade 
-        elif chance < 0.08: #chance de 8% de aparecer o diamante roxo 
+        elif chance < 0.1: #chance de 8% de aparecer o diamante roxo 
             self.tipo = "roxo"
             self.image_normal = diamond_roxo_img
             self.image_broken = diamond_roxo_broken_img
@@ -227,26 +228,26 @@ class Diamante(pygame.sprite.Sprite):
             self.tipo = "verde"
             self.image_normal = diamond_verde_img
             self.image_broken = diamond_verde_broken_img
-            velocidade_extra = 5
+            velocidade_extra = 6
         elif chance < 0.50: #chance de 50% de aparecer o diamante vermelho
             self.tipo = "vermelho"
             self.image_normal = diamond_vermelho_img
             self.image_broken = diamond_vermelho_broken_img
-            velocidade_extra = 4
+            velocidade_extra = 5.5
         elif chance < 0.75: #chance de 75% de aparecer a pedra 
             self.tipo = "pedra"
             self.image_normal = pedra_img
             self.image_broken = pedra_broken_img
-            velocidade_extra = 1
+            velocidade_extra = 5
         else: # o resto é o azul 
             self.tipo = "azul"
             self.image_normal = diamond_img
             self.image_broken = diamond_broken_img
-            velocidade_extra = 2
+            velocidade_extra = 3
 
         self.image = self.image_normal
         self.rect = self.image.get_rect()
-        self.rect.y = random.randint(20, 150)
+        self.rect.y = random.randint(10, 200)
 
         self.broken = False
         self.break_timer = 0
@@ -292,51 +293,49 @@ class Diamante(pygame.sprite.Sprite):
         else:
             som_quebra_diamante.play()
 
-
 game = True
 tela = "inicio"
 clock = pygame.time.Clock()
 FPS = 30
 
-
 diamantes = pygame.sprite.Group()
 SPAWN_EVENT = pygame.USEREVENT + 1
 pygame.time.set_timer(SPAWN_EVENT, 1200)
 
-vidas = [True, True, True]
-pontuacao = 0
+vidas = [True, True, True]  # define as vidas como verdadeiras, que mostra que o jogador tem vidas
+pontuacao = 0  # inicia a pontuação como 0
 moedas_totais = 0  # Total acumulado entre rodadas
-fase_atual = "padrao"
+fase_atual = "padrao" # Fase padrão, para evitar erro de referência antes de selecionar um mundo
+
 #POWER UPS 
 atracao_ativa = False
 
 mensagem_mundo = ""
 tempo_mensagem = 0
 
-
 while game:
-    clock.tick(FPS)
+    clock.tick(FPS) #tempo de atualização do jogo
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
 
-        if tela == "inicio":
+        if tela == "inicio": #quando a tela for a de inicio ocorrera a opção de clicar no botão para iniciar o jogo	no mundo selecionado
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if button_rect.collidepoint(event.pos):
                     tela = "selecionar_mundo"
-        elif tela == "selecionar_mundo":
+        elif tela == "selecionar_mundo": 
             if event.type == pygame.MOUSEBUTTONDOWN:
 
         # MUNDO PLANETA
                 if mundo_planeta_rect.collidepoint(event.pos):
-                    if mundos_desbloqueio["mundo_planeta"]["desbloqueado"]:
+                    if mundos_desbloqueio["mundo_planeta"]["desbloqueado"]:  # se o mundo estiver desbloqueado o jogo pode comecar 
                         tela = "jogo"
-                        fase_atual = "mundo_planeta"
-                        tempo_inicial = pygame.time.get_ticks()
+                        fase_atual = "mundo_planeta" 
+                        tempo_inicial = pygame.time.get_ticks() #inicia o tempo do jogo
                         pontuacao = 0
-                        vidas = [True, True, True]
-                        diamantes.empty()
+                        vidas = [True, True, True] #faz com que as vidas sejam verdadeiras
+                        diamantes.empty() 
     
                     elif moedas_totais >= mundos_desbloqueio["mundo_planeta"]["custo"]:
                         moedas_totais -= mundos_desbloqueio["mundo_planeta"]["custo"]
@@ -396,7 +395,8 @@ while game:
             if event.type == SPAWN_EVENT:
                 diamantes.add(Diamante())
             # Detecção contínua de clique com o mouse sobre diamantes
-        if tela == "jogo" and pygame.mouse.get_pressed()[0]:  # Botão esquerdo pressionado
+            
+        if tela == "jogo" and pygame.mouse.get_pressed()[0]:  
             mouse_pos = pygame.mouse.get_pos()
             for d in diamantes:
                 if d.rect.collidepoint(mouse_pos) and not d.broken:
@@ -431,13 +431,12 @@ while game:
             largura_texto = texto_tempo.get_width()
             window.blit(texto_tempo, ((WIDTH - largura_texto) // 2, 10))
             # Quando tempo acabar
-            if tempo_restante <= 0:
+            if tempo_restante <= 0:  #Se o tempo acabar o jogo acaba
                 moedas_totais += pontuacao
                 tela = "fim"
             
 
         if tela == "fim":
-            # moedas_totais += pontuacao
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if recomeco_rect.collidepoint(event.pos):
                     tela = "jogo"
@@ -474,30 +473,59 @@ while game:
         texto_moedas = pygame.font.SysFont(None, 36).render(str(moedas_totais), True, (255, 255, 0))
         window.blit(texto_moedas, (60, 15))
 
-    elif tela == "selecionar_mundo":
+    elif tela == "selecionar_mundo":  #Parte do codigo aonde desenha os icones na tela de selecionar o mundo 
         window.blit(background_jogo, (0, 0))
+
         # Mostrar moedas totais acumuladas no topo
         window.blit(coin_img, (10, 10))
         texto_moedas_totais = pygame.font.SysFont(None, 36).render(str(moedas_totais), True, (255, 255, 0))
         window.blit(texto_moedas_totais, (60, 15))
-        # Mundo Planeta
-        if mundos_desbloqueio["mundo_planeta"]["desbloqueado"]:
+
+        # Mundo Planeta canto superior direito 
+        if mundos_desbloqueio["mundo_planeta"]["desbloqueado"]: # Se o mundo for desbloqueado(comprado), ele ira aparecer dessa forma
             window.blit(mundo_planeta_img, mundo_planeta_rect)
         else:
-           window.blit(mundo_planeta_bloq, mundo_planeta_rect)
-        window.blit(botao_extra_img, botao_extra_rect)
+            window.blit(mundo_planeta_bloq, mundo_planeta_rect)  # planeta permanece bloqueado 
+            bloqueado = not mundos_desbloqueio["mundo_planeta"]["desbloqueado"] # define o que seria o mundo bloqueado
+            if bloqueado: # se ele estiver bloqueado a moeda aparece
+                x1, y1 = mundo_planeta_rect.center # centraliza as moedas com a posição dos mundos
+                valor1 = fonte_moeda.render("500", True, (255, 255, 0))  # faz a moeda do tamanho e valor do mundo
+                larg_val1 = valor1.get_width()  # Pega a largura do que sera escrito
+                alt_val1 = valor1.get_height()  # Pega a altura do que sera escrito 
+                window.blit(valor1, (x1 - larg_val1 //2, y1- 130))  # centraliza o valor em cima da moeda
+                window.blit(coin_img, (x1 - larg_val1 //2 + larg_val1 + 5,  y1- 130 + alt_val1//2 - 20))  #desenha moeda em cima do mundo 
+            window.blit(botao_extra_img, botao_extra_rect)
 
-    # Mundo Místico
+    # Mundo Místico, canto inferior direito
         if mundos_desbloqueio["mundo_mistico"]["desbloqueado"]:
             window.blit(mundo_mistico_img, mundo_mistico_rect)
         else:
             window.blit(mundo_mistico_bloq, mundo_mistico_rect)
-
-    # Mundo Verde
+            bloqueado = not mundos_desbloqueio["mundo_planeta"]["desbloqueado"]
+            if bloqueado:
+                x2, y2 = mundo_planeta_rect.center 
+                val2 = fonte_moeda.render("250", True, (255, 255, 0))
+                alt_val2 = val2.get_height()  
+                largura_val2 = val2.get_width() 
+                window.blit(coin_img, (x2 + largura_val2//2+5, y2 + 100 + alt_val2//2 - 20)) 
+                window.blit(val2, (x2 - largura_val2//2, y2 + 100))
+            window.blit(botao_extra_img, botao_extra_rect)
+            
+    # Mundo Verde canto inferior esquerdo
         if mundos_desbloqueio["mundo_verde"]["desbloqueado"]:
             window.blit(mundo_verde_img, mundo_verde_rect)
         else:
             window.blit(mundo_verde_bloq, mundo_verde_rect)
+            bloqueado = not mundos_desbloqueio["mundo_planeta"]["desbloqueado"] 
+            if bloqueado: 
+                x3, y3 = mundo_verde_rect.center
+                val3 = fonte_moeda.render("1000", True, (255, 255, 0))
+                alt_val3 = val3.get_height()  
+                larg_val3 = val3.get_width()  
+                base3 = y3 + 100
+                window.blit(val3, (x3- larg_val3//2, base3))
+                window.blit(coin_img, (x3+larg_val3//2+5, base3+alt_val3//2-20))  
+            window.blit(botao_extra_img, botao_extra_rect)
 
     # Mundo Mina (sempre desbloqueado)
         window.blit(mundo1_img, mundo1_rect)
